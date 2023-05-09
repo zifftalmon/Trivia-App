74ee7e4645cfed1ebb05af68920434c86d087378
ziv talmon <zivtalmon89@gmail.com>
2023-05-09 13:46:33 +0300

    promise for trivia questions resolved and mapped to the DOM

--- src/components/Trivia.tsx
+++ src/components/Trivia.tsx
@@ -3,16 +3,29 @@ import React from "react";
 
 
 const Trivia:React.FC = () => {
+
+    const [questions, setQuestions]:any[] = useState([]);
+
     useEffect(() => {
         fetch('https://opentdb.com/api.php?amount=10&category=9')
-        .then(req => req.json())
-        .then(questions => console.log(questions))
-    },[])
+            .then(req => req.json())
+            .then(data => setQuestions(data.results))
+    },[]);
+    // console.log(questions);
+    
     return(
         <div>
-
+            {questions.map((item: any, i:number) => {
+                return (
+                    <div key={i}>
+                        <h2>{i+1}. {item.question}</h2>
+                    </div>
+                )
+                
+            })}
         </div>
     )
 }
 
+// setQuestions((questions: string) => [...questions, trivia])
 export default Trivia
